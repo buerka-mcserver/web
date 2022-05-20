92b9c2f31f9d6cfe921821f62f41a517a7c21c5d
Litfa <admin@litf.com.cn>
2022-05-20 15:51:29 +0800

    feat(components/HomdCards/Advantages): 优势详情

--- src/components/HomdCards/Advantages.vue
+++ src/components/HomdCards/Advantages.vue
@@ -2,31 +2,33 @@
 import UserPreference from '@/components/Illustrations/UserPreference.vue'
 import Conversion from '@/components/Illustrations/Conversion.vue'
 import StandardDelivery from '@/components/Illustrations/StandardDelivery.vue'
+import AdvantagesIcon from './AdvantagesIcon.vue'
+import { ref } from 'vue'
+
+const text = ref('我们拥有高性能的家里云主机，支持ipv4与ipv6，拒绝任何卡顿！给您带来更棒，更畅快的游戏体验。')
+const setText = (t: string) => {
+  text.value = t
+}
 </script>
 
 <template>
   <div class="advantages">
     <h1>我们的优势</h1>
     <div class="icons">
-      <div class="item">
-        <div class="icon">
-          <UserPreference></UserPreference>
-        </div>
-        <span>体验好</span>
-      </div>
-      <div class="item">
-        <div class="icon">
-          <Conversion style="transform: translateX(8px);"></Conversion>
-        </div>
-        <span>高性能</span>
-      </div>
-      <div class="item">
-        <div class="icon">
-          <StandardDelivery></StandardDelivery>
-        </div>
-        <span>互通服</span>
-      </div>
+      <AdvantagesIcon text="体验好" @hover="setText('游戏体验好')">
+        <UserPreference></UserPreference>
+      </AdvantagesIcon>
+      <AdvantagesIcon
+        text="高性能"
+        @hover="setText('我们拥有高性能的家里云主机，支持ipv4与ipv6，拒绝任何卡顿！给您带来更棒，更畅快的游戏体验。')"
+      >
+        <Conversion style="transform: translateX(8px);"></Conversion>
+      </AdvantagesIcon>
+      <AdvantagesIcon text="互通服" @hover="setText('同时支持je，be进入')">
+        <Conversion style="transform: translateX(8px);"></Conversion>
+      </AdvantagesIcon>
     </div>
+    <div class="text">{{ text }}</div>
   </div>
 </template>
 
@@ -34,31 +36,24 @@ import StandardDelivery from '@/components/Illustrations/StandardDelivery.vue'
 .advantages {
   width: 100%;
   background-color: #eef0f5;
+  padding: 50px 0;
   .icons {
+    max-width: 800px;
+    margin: 10px auto;
     width: 100%;
     display: flex;
     align-items: center;
-    justify-content: space-between;
+    justify-content: space-around;
+    cursor: pointer;
+  }
+  .text {
     max-width: 800px;
-    margin: 10px auto;
-    .item {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      transition: all 0.3s;
-      border-radius: 10px;
-      &:hover {
-        background-color: #fff;
-      }
-      .icon {
-        display: flex;
-        font-size: 20px;
-        svg {
-          width: 120px;
-          height: 120px;
-        }
-      }
-    }
+    min-height: 3em;
+    margin-top: 20px;
+    transform: translateX(-50%);
+    margin-left: 52%;
+    width: 100%;
+    font-size: 20px;
   }
 }
 </style>
---
+++ src/components/HomdCards/AdvantagesIcon.vue
@@ -0,0 +1,41 @@
+<script lang="ts" setup>
+const props = defineProps({
+  text: String,
+  desc: String
+})
+const emits = defineEmits(['hover'])
+</script>
+
+<template>
+  <div class="item" @mouseover="emits('hover',)">
+    <div class="icon">
+      <slot></slot>
+    </div>
+    <span>{{ text }}</span>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  transition: all 0.3s;
+  border-radius: 10px;
+  &:hover {
+    background-color: #fff;
+  }
+  .icon {
+    display: flex;
+    font-size: 20px;
+    transition: all 0.3s;
+    &:hover {
+      transform: translateY(5px);
+    }
+    :deep(svg) {
+      width: 120px;
+      height: 120px;
+    }
+  }
+}
+</style>
